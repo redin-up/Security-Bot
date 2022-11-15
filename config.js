7187589da64b7955279d4739c4f9dd8198e828c6
redin-up <114192373+redin-up@users.noreply.github.com>
2022-11-15 10:20:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     defaultPrefix: 's!',
-    ownerID: '922530000618528778',
+    ownerID: '905863161457025055',
     _limits: 'The following are defaults.',
     adminCanChangeLimits: true,
     limits: {
